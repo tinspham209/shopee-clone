14cd94688d3768352e9d28c57489291242170e47
tinspham209 <tindeptrai1@gmail.com>
2021-01-12 23:05:41 +0700

    update logic when change product quantily

--- index.js
+++ index.js
@@ -71,10 +71,11 @@ $(document).ready(function () {
 	// product qty section
 	let $qty_up = $(".qty .qty-up");
 	let $qty_down = $(".qty .qty-down");
-	let $input = $(".qty .qty_input");
 
 	// click on qty up btn
 	$qty_up.click(function (e) {
+		let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+
 		if ($input.val() >= 1 && $input.val() <= 9) {
 			$input.val(function (i, oldVal) {
 				return ++oldVal;
@@ -84,6 +85,7 @@ $(document).ready(function () {
 
 	// click on qty down btn
 	$qty_down.click(function (e) {
+		let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
 		if ($input.val() > 1 && $input.val() <= 10) {
 			$input.val(function (i, oldVal) {
 				return --oldVal;
