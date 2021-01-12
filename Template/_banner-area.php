36f968124bbe393dd6993b40a64eaaa94234956c
tinspham209 <tindeptrai1@gmail.com>
2021-01-13 00:53:00 +0700

    refactor homepage

---
+++ Template/_banner-area.php
@@ -0,0 +1,15 @@
+<!-- Owl-carousel -->
+<div class="section" id="banner-area">
+    <div class="owl-carousel owl-theme">
+        <div class="item">
+            <img src="./assets/Banner1.png" alt="banner1" />
+        </div>
+        <div class="item">
+            <img src="./assets/Banner2.png" alt="banner2" />
+        </div>
+        <div class="item">
+            <img src="./assets/Banner1.png" alt="banner3" />
+        </div>
+    </div>
+</div>
+<!-- Owl-carousel -->
