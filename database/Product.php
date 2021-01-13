fd658882501fe477e5c58c514501ddb8734d9790
tinspham209 <tindeptrai1@gmail.com>
2021-01-13 14:17:49 +0700

    add: getProduct func

--- database/Product.php
+++ database/Product.php
@@ -26,4 +26,19 @@ class Product
 
         return $resultArray;
     }
+
+    public function getProduct($item_id = null, $table = 'product')
+    {
+        if (isset($item_id)) {
+            $result = $this->db->con->query("SELECT * FROM {$table} WHERE item_id={$item_id}");
+            $resultArray = array();
+
+            // fetch product data one by one
+            while ($item = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                $resultArray[] = $item;
+            }
+
+            return $resultArray;
+        }
+    }
 }
