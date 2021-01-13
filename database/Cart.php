3bcb9c016665b55aa780935f013897d456ad0600
tinspham209 <tindeptrai1@gmail.com>
2021-01-13 13:10:48 +0700

    add: Cart Controller

---
+++ database/Cart.php
@@ -0,0 +1,43 @@
+<?php
+
+
+class Cart
+{
+    public $db = null;
+
+    public function __construct(DBController $db)
+    {
+        if(!isset($db->con)){
+            return null;
+        }
+        $this->db = $db;
+    }
+
+    public function insertIntoCart($params = null, $table = "cart"){
+        if($this->db->con != null){
+            if($params != null) {
+                $columns = implode(',', array_keys($params));
+                $values = implode(',', array_values($params));
+
+                $query_string = sprintf("INSERT INTO %s(%s) VALUES(%s)", $table, $columns, $values);
+                $result = $this->db->con->query($query_string);
+                return $result;
+            }
+        }
+    }
+
+    public function addToCart($userid, $itemid){
+        if(isset($userid) && isset($itemid)){
+            $params = array(
+                "user_id" => $userid,
+                "item_id" => $itemid
+            );
+        }
+        $result = $this->insertIntoCart($params);
+        if($result){
+            // reload page
+            header("Location:" . $_SERVER['PHP_SELF']);
+        }
+    }
+
+}
