548e2841dba62d0710776642282068c281a1c1ea
tinspham209 <tindeptrai1@gmail.com>
2021-01-13 13:11:17 +0700

    Init Card db

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 //        require MySQL Connection
 require('database/DBController.php');
 require('database/Product.php');
+require('database/Cart.php');
 
 // DBController object
 $db = new DBController();
@@ -9,4 +10,4 @@ $db = new DBController();
 $product = new Product($db);
 $product_shuffle = $product->getData();
 
-print_r($product->getData());
+$Cart = new Cart($db);
