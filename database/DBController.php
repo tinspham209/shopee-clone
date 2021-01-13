a87f0906ceac05584287a3036dac6b8728e54b7f
tinspham209 <tindeptrai1@gmail.com>
2021-01-13 11:32:01 +0700

    Fix: update connection DBController

--- database/DBController.php
+++ database/DBController.php
@@ -3,30 +3,32 @@
 
 class DBController
 {
-//    DB Connection Properties
+    // Database Connection Properties
     public $con = null;
     protected $host = 'localhost:8899';
-    protected $user = "root";
-    protected $password = "";
-
-//    connection property
+    protected $user = 'root';
+    protected $password = '';
     protected $database = "shopee";
 
-//    call constructor
+    // call constructor
+
     public function __construct()
     {
         $this->con = mysqli_connect($this->host, $this->user, $this->password, $this->database);
         if ($this->con->connect_error) {
-            echo "Fail" . $this->con->connect_error;
+            echo "Fail " . $this->con->connect_error;
         }
     }
 
-    public function __destruct(){
+    public function __destruct()
+    {
         $this->closeConnection();
     }
 
-    protected function closeConnection(){
-        if($this->con != null){
+    // for mysqli closing connection
+    protected function closeConnection()
+    {
+        if ($this->con != null) {
             $this->con->close();
             $this->con = null;
         }
