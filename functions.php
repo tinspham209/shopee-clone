75dd3c9aa492214ece5bcf7d71adbbdd9033d1c6
tinspham209 <tindeptrai1@gmail.com>
2021-01-13 11:32:42 +0700

    add: connect product table

--- functions.php
+++ functions.php
@@ -1,3 +1,11 @@
 <?php
 //        require MySQL Connection
 require('database/DBController.php');
+require('database/Product.php');
+
+// DBController object
+$db = new DBController();
+
+$product = new Product($db);
+
+print_r($product->getData());
