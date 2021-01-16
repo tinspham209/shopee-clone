1458a4b67b819b453decb06f1462ca20b8d7aef7
tinspham209 <tindeptrai1@gmail.com>
2021-01-16 12:30:24 +0700

    fix bug

--- Template/_banner-area.php
+++ Template/_banner-area.php
@@ -1,15 +1,15 @@
 <!-- Owl-carousel -->
-<div class="section" id="banner-area">
+<section id="banner-area">
     <div class="owl-carousel owl-theme">
         <div class="item">
-            <img src="./assets/Banner1.png" alt="banner1" />
+            <img src="./assets/Banner1.png" alt="Banner1">
         </div>
         <div class="item">
-            <img src="./assets/Banner2.png" alt="banner2" />
+            <img src="./assets/Banner2.png" alt="Banner2">
         </div>
         <div class="item">
-            <img src="./assets/Banner1.png" alt="banner3" />
+            <img src="./assets/Banner1.png" alt="Banner3">
         </div>
     </div>
-</div>
-<!-- Owl-carousel -->
+</section>
+<!-- !Owl-carousel -->
