7d2295e4302760a8cdbda59bc44d25f6dedda23e
tinspham209 <tindeptrai1@gmail.com>
2021-01-16 12:32:26 +0700

    add: function handle quantily item

--- index.js
+++ index.js
@@ -1,95 +1,136 @@
 $(document).ready(function () {
-    // Banner carousel
-    $("#banner-area .owl-carousel").owlCarousel({
-        dots: true,
-        items: 1,
-    });
+	// banner owl carousel
+	$("#banner-area .owl-carousel").owlCarousel({
+		dots: true,
+		items: 1,
+	});
 
-    // top sale
-    $("#top-sale .owl-carousel").owlCarousel({
-        loop: true,
-        nav: true,
-        dots: false,
-        responsive: {
-            0: {
-                items: 1,
-            },
-            600: {
-                items: 3,
-            },
-            1000: {
-                items: 5,
-            },
-        },
-    });
+	// top sale owl carousel
+	$("#top-sale .owl-carousel").owlCarousel({
+		loop: true,
+		nav: true,
+		dots: false,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			600: {
+				items: 3,
+			},
+			1000: {
+				items: 5,
+			},
+		},
+	});
 
-    // isotope filter
-    var $grid = $(".grid").isotope({
-        itemSelector: ".grid-item",
-        layoutMode: "fitRows",
-    });
+	// isotope filter
+	var $grid = $(".grid").isotope({
+		itemSelector: ".grid-item",
+		layoutMode: "fitRows",
+	});
 
-    // Filter items on button click
-    $(".button-group").on("click", "button", function () {
-        var filterValue = $(this).attr("data-filter");
-        $grid.isotope({ filter: filterValue });
-    });
+	// filter items on button click
+	$(".button-group").on("click", "button", function () {
+		var filterValue = $(this).attr("data-filter");
+		$grid.isotope({ filter: filterValue });
+	});
 
-    // new phones carousel
-    $("#new-phones .owl-carousel").owlCarousel({
-        loop: true,
-        nav: false,
-        dots: true,
-        responsive: {
-            0: {
-                items: 1,
-            },
-            600: {
-                items: 3,
-            },
-            1000: {
-                items: 5,
-            },
-        },
-    });
+	// new phones owl carousel
+	$("#new-phones .owl-carousel").owlCarousel({
+		loop: true,
+		nav: false,
+		dots: true,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			600: {
+				items: 3,
+			},
+			1000: {
+				items: 5,
+			},
+		},
+	});
 
-    // blogs carousel
-    $("#blogs .owl-carousel").owlCarousel({
-        loop: true,
-        nav: false,
-        dots: true,
-        responsive: {
-            0: {
-                items: 1,
-            },
-            600: {
-                items: 3,
-            },
-        },
-    });
+	// blogs owl carousel
+	$("#blogs .owl-carousel").owlCarousel({
+		loop: true,
+		nav: false,
+		dots: true,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			600: {
+				items: 3,
+			},
+		},
+	});
 
-    // product qty section
-    let $qty_up = $(".qty .qty-up");
-    let $qty_down = $(".qty .qty-down");
+	// product qty section
+	let $qty_up = $(".qty .qty-up");
+	let $qty_down = $(".qty .qty-down");
+	let $deal_price = $("#deal-price");
+	// let $input = $(".qty .qty_input");
 
-    // click on qty up btn
-    $qty_up.click(function (e) {
-        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+	// click on qty up button
+	$qty_up.click(function (e) {
+		let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+		let $price = $(`.product_price[data-id='${$(this).data("id")}']`);
 
-        if ($input.val() >= 1 && $input.val() <= 9) {
-            $input.val(function (i, oldVal) {
-                return ++oldVal;
-            });
-        }
-    });
+		// change product price using ajax call
+		$.ajax({
+			url: "template/ajax.php",
+			type: "post",
+			data: { itemid: $(this).data("id") },
+			success: function (result) {
+				let obj = JSON.parse(result);
+				let item_price = obj[0]["item_price"];
 
-    // click on qty down btn
-    $qty_down.click(function (e) {
-        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
-        if ($input.val() > 1 && $input.val() <= 10) {
-            $input.val(function (i, oldVal) {
-                return --oldVal;
-            });
-        }
-    });
+				if ($input.val() >= 1 && $input.val() <= 9) {
+					$input.val(function (i, oldval) {
+						return ++oldval;
+					});
+
+					// increase price of the product
+					$price.text(parseInt(item_price * $input.val()).toFixed(2));
+
+					// set subtotal price
+					let subtotal = parseInt($deal_price.text()) + parseInt(item_price);
+					$deal_price.text(subtotal.toFixed(2));
+				}
+			},
+		}); // closing ajax request
+	}); // closing qty up button
+
+	// click on qty down button
+	$qty_down.click(function (e) {
+		let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+		let $price = $(`.product_price[data-id='${$(this).data("id")}']`);
+
+		// change product price using ajax call
+		$.ajax({
+			url: "template/ajax.php",
+			type: "post",
+			data: { itemid: $(this).data("id") },
+			success: function (result) {
+				let obj = JSON.parse(result);
+				let item_price = obj[0]["item_price"];
+
+				if ($input.val() > 1 && $input.val() <= 10) {
+					$input.val(function (i, oldval) {
+						return --oldval;
+					});
+
+					// increase price of the product
+					$price.text(parseInt(item_price * $input.val()).toFixed(2));
+
+					// set subtotal price
+					let subtotal = parseInt($deal_price.text()) - parseInt(item_price);
+					$deal_price.text(subtotal.toFixed(2));
+				}
+			},
+		}); // closing ajax request
+	}); // closing qty down button
 });
